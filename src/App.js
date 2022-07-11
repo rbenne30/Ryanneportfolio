39571289f22d54d39f8f8570b29155a9c0b3d6ab
rbenne30 <ryannebennett1@outlook.com>
2022-07-11 10:46:49 -0400

    updated package.json

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
-import './App.css'
-import React, { useState } from 'react';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import NavTabs from './components/pages/NavTabs';
-import Footer from './components/pages/Footer';
-import Project from './components/pages/Project';
-import Resume from './components/pages/Resume';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [getState, setState] = useState('home');
-
-  const rendering = choice => {
-    switch (choice) {
-      case 'home':
-        return <Home />
-      case 'project':
-        return <Project />
-      case 'about':
-        return <About />
-      case 'resume':
-        return <Resume />
-    }
-  }
-
   return (
-    <>
-      <NavTabs setState={setState} />
-      {rendering(getState)}
-      <Footer />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-
 }
 
 export default App;
